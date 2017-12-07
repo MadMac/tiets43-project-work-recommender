f093a943f6033ed99353e789a4abc2485ee6a5bf
Markus Hallfors <markus@hallfors.com>
2017-12-07 23:34:25 +0200

    Add game ratings and get recommendations based on those ratings

--- page.py
+++ page.py
@@ -1,13 +1,46 @@
 import numpy as np
+import json
+from collections import OrderedDict
 from game_recommender import GameRecommender
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def main_page():
+	gameRecommender = GameRecommender()
 
-	return render_template('index.html')
+	allGames = OrderedDict(sorted(gameRecommender.game_indexes.items(), key=lambda t: t[0]))
+	# print (gameRecommender.game_indexes)
+	return render_template('index.html', games=allGames)
+
+@app.route("/get-recommendation", methods=["POST"])
+def get_recommendation():
+	gameRecommender = GameRecommender()
+	allGames = request.get_json(force=True)
+	print (allGames)
+
+	gamesRow = []
+
+	for i in range(len(gameRecommender.games_list)):
+		selectedGame = None
+
+		for game in allGames:
+			# print(game['id'])
+			if i == int(game['id']):
+				selectedGame = game
+				# print (game)
+	
+		if selectedGame is None:
+			gamesRow.append(0)
+		else:
+			gamesRow.append(int(selectedGame['rating']))
+	print (len(gamesRow))
+	print (len(gameRecommender.game_indexes))
+	r_games = gameRecommender.recommendations_by_vector(gamesRow, 10)
+
+
+	return "test"
 
 if __name__ == "__main__":
     app.run()
--- static/script.js
+++ static/script.js
@@ -1,3 +1,46 @@
 $(document).ready(function() {
     $('.game-selector').select2();
 });
+
+var allGames = [];
+
+function addGame() {
+    gameId = $('#game-selector').val();
+    gameName = $('#game-selector option:selected').text();
+    gameRating = $('#rating-selector').val();
+
+    var alreadyIn = false;
+
+    for(var i = 0; i < allGames.length; ++i) {
+        if (allGames[i][1] == gameId) {
+            alreadyIn = true;
+            allGames[i] = [gameName, gameId, gameRating];
+        }
+    }
+
+    if (!alreadyIn) {
+        allGames.push([gameName, gameId, gameRating]);
+    }
+
+    $('#list-games').empty();
+    for (var i = 0; i < allGames.length; ++i) {
+        $('#list-games').append("<div>" + allGames[i][0] + ": " + allGames[i][2] + "</div>");
+    }
+    
+}
+
+function getRecommendation() {
+
+    jsonData = new Array();
+    for (var i = 0; i < allGames.length; ++i) {
+        gameData = new Object();
+        gameData.name = allGames[i][0];
+        gameData.id = allGames[i][1];
+        gameData.rating = allGames[i][2];
+        jsonData.push(gameData);
+    }
+
+    $.post("/get-recommendation", JSON.stringify(jsonData), function( data ) {
+        console.log(data);
+    });
+}
